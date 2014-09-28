6a7c491494b8f105fcebd4774c310adf0cb220d6
Open-Steps.org <contact@open-steps.org>
2014-09-28 19:25:24 +0200

    Removed old scripts. Now bootstrap.sh does not check envvar for travis/production provisioning conditional

--- deployment-scripts/vagrant/bootstrap.sh
+++ deployment-scripts/vagrant/bootstrap.sh
@@ -109,135 +109,130 @@ cd /usr/lib/ckan/default/src/ckanext-odm_theme/
 git clone https://github.com/OpenDevelopmentMekong/ckanext-odm_theme.git .
 python setup.py develop
 
-# THE LINES FROM HERE SHOULD NOT BE RUN BY TRAVIS
-if [[ -z "$RUN_ON_TRAVIS" ]]
-then
+echo 'Installing other extensions'
+echo '----------------------------------'
 
-	echo 'Installing other extensions'
-	echo '----------------------------------'
+# Install extra packages: libxml2-dev and libxslt-dev are needed by ckanext-spatial, apache2/nginx for deployment
+sudo apt-get install --yes libxml2-dev libxslt-dev
+sudo apt-get install --yes apache2 libapache2-mod-wsgi
+sudo apt-get install --yes nginx
+# sudo apt-get install -y postfix -> This breaks the script!!
 
-	# Install extra packages: libxml2-dev and libxslt-dev are needed by ckanext-spatial, apache2/nginx for deployment
-	sudo apt-get install --yes libxml2-dev libxslt-dev
-	sudo apt-get install --yes apache2 libapache2-mod-wsgi
-	sudo apt-get install --yes nginx
-	# sudo apt-get install -y postfix -> This breaks the script!!
+echo 'Install FileStore'
+echo '----------------------------------'
+sudo mkdir -p /var/lib/ckan/default
+sudo chown www-data /var/lib/ckan/default
+sudo chmod u+rwx /var/lib/ckan/default
 
-	echo 'Install FileStore'
-	echo '----------------------------------'
-	sudo mkdir -p /var/lib/ckan/default
-	sudo chown www-data /var/lib/ckan/default
-	sudo chmod u+rwx /var/lib/ckan/default
+echo 'OPTIONAL: Install Datapusher extension'
+echo '----------------------------------'
+sudo apt-get install python-dev python-virtualenv build-essential libxslt1-dev libxml2-dev git
+sudo virtualenv /usr/lib/ckan/datapusher
+sudo mkdir /usr/lib/ckan/datapusher/src
+cd /usr/lib/ckan/datapusher/src
+sudo git clone -b stable https://github.com/ckan/datapusher.git
+cd datapusher
+sudo /usr/lib/ckan/datapusher/bin/pip install -r requirements.txt
+sudo /usr/lib/ckan/datapusher/bin/python setup.py develop
+sudo cp deployment/datapusher /etc/apache2/sites-available/
+sudo cp deployment/datapusher.wsgi /etc/ckan/
+sudo cp deployment/datapusher_settings.py /etc/ckan/
+sudo sh -c 'echo "NameVirtualHost *:8800" >> /etc/apache2/ports.conf'
+sudo sh -c 'echo "Listen 8800" >> /etc/apache2/ports.conf'
+sudo a2ensite datapusher
 
-	# echo 'OPTIONAL: Install Datapusher extension'
-	# echo '----------------------------------'
-	# sudo apt-get install python-dev python-virtualenv build-essential libxslt1-dev libxml2-dev git
-	# sudo virtualenv /usr/lib/ckan/datapusher
-	# sudo mkdir /usr/lib/ckan/datapusher/src
-	# cd /usr/lib/ckan/datapusher/src
-	# sudo git clone -b stable https://github.com/ckan/datapusher.git
-	# cd datapusher
-	# sudo /usr/lib/ckan/datapusher/bin/pip install -r requirements.txt
-	# sudo /usr/lib/ckan/datapusher/bin/python setup.py develop
-	# sudo cp deployment/datapusher /etc/apache2/sites-available/
-	# sudo cp deployment/datapusher.wsgi /etc/ckan/
-	# sudo cp deployment/datapusher_settings.py /etc/ckan/
-	# sudo sh -c 'echo "NameVirtualHost *:8800" >> /etc/apache2/ports.conf'
-	# sudo sh -c 'echo "Listen 8800" >> /etc/apache2/ports.conf'
-	# sudo a2ensite datapusher
+# go back to ckan dir
+cd /usr/lib/ckan/default/
 
-	# go back to ckan dir
-	cd /usr/lib/ckan/default/
+echo 'Install Pages extension'
+echo '----------------------------------'
+sudo pip install -e 'git+https://github.com/ckan/ckanext-pages.git#egg=ckanext-pages'
+cd /usr/lib/ckan/default/src/ckanext-pages
+python setup.py develop
 
-	echo 'Install Pages extension'
-	echo '----------------------------------'
-	sudo pip install -e 'git+https://github.com/ckan/ckanext-pages.git#egg=ckanext-pages'
-	cd /usr/lib/ckan/default/src/ckanext-pages
-	python setup.py develop
+# go back to ckan dir
+cd /usr/lib/ckan/default/
 
-	# go back to ckan dir
-	cd /usr/lib/ckan/default/
+echo 'Install ckanext-spatial extension (Just for Preview for spatial formats)'
+echo '----------------------------------'
+sudo pip install -e "git+https://github.com/okfn/ckanext-spatial.git@stable#egg=ckanext-spatial"
+cd /usr/lib/ckan/default/src/ckanext-spatial/
+sudo pip install -r pip-requirements.txt
+sudo python setup.py develop
 
-	echo 'Install ckanext-spatial extension (Just for Preview for spatial formats)'
-	echo '----------------------------------'
-	sudo pip install -e "git+https://github.com/okfn/ckanext-spatial.git@stable#egg=ckanext-spatial"
-	cd /usr/lib/ckan/default/src/ckanext-spatial/
-	sudo pip install -r pip-requirements.txt
-	sudo python setup.py develop
+# go back to ckan dir
+cd /usr/lib/ckan/default/
 
-	# go back to ckan dir
-	cd /usr/lib/ckan/default/
+echo 'Install Google Analytics extension'
+echo '----------------------------------'
+sudo pip install -e  "git+https://github.com/ckan/ckanext-googleanalytics.git#egg=ckanext-googleanalytics"
+cd /usr/lib/ckan/default/src/ckanext-googleanalytics
+sudo python setup.py develop
 
-	echo 'Install Google Analytics extension'
-	echo '----------------------------------'
-	sudo pip install -e  "git+https://github.com/ckan/ckanext-googleanalytics.git#egg=ckanext-googleanalytics"
-	cd /usr/lib/ckan/default/src/ckanext-googleanalytics
-	sudo python setup.py develop
+echo 'Deployment: Following this http://docs.ckan.org/en/latest/maintaining/installing/deployment.html'
+echo '----------------------------------'
+cd /etc/ckan/default
+sudo wget -O production.ini https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/production.ini?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvcHJvZHVjdGlvbi5pbmkiLCJleHBpcmVzIjoxNDEyMjU2NjM3fQ%3D%3D--2944fad9f8ec655d9d0e47c114b15ee00ed303c3
 
-	echo 'Deployment: Following this http://docs.ckan.org/en/latest/maintaining/installing/deployment.html'
-	echo '----------------------------------'
-	cd /etc/ckan/default
-	sudo wget -O production.ini https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/production.ini?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvcHJvZHVjdGlvbi5pbmkiLCJleHBpcmVzIjoxNDEyMjU2NjM3fQ%3D%3D--2944fad9f8ec655d9d0e47c114b15ee00ed303c3
+echo 'Set up the DataStore'
+echo '----------------------------------'
+# Pasting password into ~/.pgpass to bypass password prompt while running createuser for datastore_default
+# TODO: Hide password
+#echo '*:*:*:datastore_default:odmadmin' >> ~/.pgpass
+#sudo -u postgres createuser --no-password -S -D -R datastore_default
+sudo -u postgres createuser -S -D -R datastore_default
+sudo -u postgres createdb -O ckan_default datastore_default -E utf-8
+sudo /usr/lib/ckan/default/bin/paster --plugin=ckan datastore set-permissions postgres --config=/etc/ckan/default/production.ini
 
-	echo 'Set up the DataStore'
-	echo '----------------------------------'
-	# Pasting password into ~/.pgpass to bypass password prompt while running createuser for datastore_default
-	# TODO: Hide password
-	#echo '*:*:*:datastore_default:odmadmin' >> ~/.pgpass
-	#sudo -u postgres createuser --no-password -S -D -R datastore_default
-	sudo -u postgres createuser -S -D -R datastore_default
-	sudo -u postgres createdb -O ckan_default datastore_default -E utf-8
-	sudo /usr/lib/ckan/default/bin/paster --plugin=ckan datastore set-permissions postgres --config=/etc/ckan/default/production.ini
+echo 'Download WSGI script file from odm-scripting repo'
+echo '----------------------------------'
+cd /etc/ckan/default
+sudo wget -O apache.wsgi https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/apache.wsgi?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvYXBhY2hlLndzZ2kiLCJleHBpcmVzIjoxNDEyMjUyNjAxfQ%3D%3D--c7f96687788b91c7f9017ba4ad179218abebac77
 
-	echo 'Download WSGI script file from odm-scripting repo'
-	echo '----------------------------------'
-	cd /etc/ckan/default
-	sudo wget -O apache.wsgi https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/apache.wsgi?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvYXBhY2hlLndzZ2kiLCJleHBpcmVzIjoxNDEyMjUyNjAxfQ%3D%3D--c7f96687788b91c7f9017ba4ad179218abebac77
+echo 'Download the Apache config file'
+echo '----------------------------------'
+cd /etc/apache2/sites-available/
+sudo wget -O ckan_default https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/apache_ckan_default?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvYXBhY2hlX2NrYW5fZGVmYXVsdCIsImV4cGlyZXMiOjE0MTIyNTI2MjN9--0c0bda819ff3a99eeaacdc56e0de74a730732d26
 
-	echo 'Download the Apache config file'
-	echo '----------------------------------'
-	cd /etc/apache2/sites-available/
-	sudo wget -O ckan_default https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/apache_ckan_default?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvYXBhY2hlX2NrYW5fZGVmYXVsdCIsImV4cGlyZXMiOjE0MTIyNTI2MjN9--0c0bda819ff3a99eeaacdc56e0de74a730732d26
+echo 'Download the Apache ports.conf file'
+echo '----------------------------------'
+cd /etc/apache2/
+sudo wget -O ports.conf https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/ports.conf?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvcG9ydHMuY29uZiIsImV4cGlyZXMiOjE0MTIyNTI2NTZ9--452162a6c1af3348ce9b008df3b4c3d292daee2e
 
-	echo 'Download the Apache ports.conf file'
-	echo '----------------------------------'
-	cd /etc/apache2/
-	sudo wget -O ports.conf https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/ports.conf?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvcG9ydHMuY29uZiIsImV4cGlyZXMiOjE0MTIyNTI2NTZ9--452162a6c1af3348ce9b008df3b4c3d292daee2e
+echo 'Download the Nginx config file'
+echo '----------------------------------'
+cd /etc/nginx/sites-available/
+sudo wget -O ckan_default https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/nginx_ckan_default?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvbmdpbnhfY2thbl9kZWZhdWx0IiwiZXhwaXJlcyI6MTQxMjI1MjY4M30%3D--5e3df80d913038ca6c8ca5871938d95dd405ed60
 
-	echo 'Download the Nginx config file'
-	echo '----------------------------------'
-	cd /etc/nginx/sites-available/
-	sudo wget -O ckan_default https://raw.githubusercontent.com/OpenDevelopmentMekong/odm-scripting/deployment-scripts/deployment-scripts/ckan_deployment/nginx_ckan_default?token=384894__eyJzY29wZSI6IlJhd0Jsb2I6T3BlbkRldmVsb3BtZW50TWVrb25nL29kbS1zY3JpcHRpbmcvZGVwbG95bWVudC1zY3JpcHRzL2RlcGxveW1lbnQtc2NyaXB0cy9ja2FuX2RlcGxveW1lbnQvbmdpbnhfY2thbl9kZWZhdWx0IiwiZXhwaXJlcyI6MTQxMjI1MjY4M30%3D--5e3df80d913038ca6c8ca5871938d95dd405ed60
+echo 'Enable CKAN site'
+echo '----------------------------------'
+sudo a2ensite ckan_default
+sudo ln -s /etc/nginx/sites-available/ckan_default /etc/nginx/sites-enabled/ckan_default
 
-	echo 'Enable CKAN site'
-	echo '----------------------------------'
-	sudo a2ensite ckan_default
-	sudo ln -s /etc/nginx/sites-available/ckan_default /etc/nginx/sites-enabled/ckan_default
+echo 'Disable apache default site'
+echo '----------------------------------'
+sudo a2dissite default
 
-	echo 'Disable apache default site'
-	echo '----------------------------------'
-	sudo a2dissite default
+echo 'Restart apache and nginx'
+echo '----------------------------------'
+sudo service apache2 reload
+sudo service nginx reload
 
-	echo 'Restart apache and nginx'
-	echo '----------------------------------'
-	sudo service apache2 reload
-	sudo service nginx reload
+echo 'Configure Test environment'
+echo '----------------------------------'
+pip install -r /usr/lib/ckan/default/src/ckan/dev-requirements.txt
+sudo -u postgres createdb -O ckan_default ckan_test -E utf-8
+sudo -u postgres createdb -O ckan_default datastore_test -E utf-8
+sudo /usr/lib/ckan/default/bin/paster --plugin=ckan datastore set-permissions postgres -c /usr/lib/ckan/default/src/ckan/test-core.ini
 
-	echo 'Configure Test environment'
-	echo '----------------------------------'
-	pip install -r /usr/lib/ckan/default/src/ckan/dev-requirements.txt
-	sudo -u postgres createdb -O ckan_default ckan_test -E utf-8
-	sudo -u postgres createdb -O ckan_default datastore_test -E utf-8
-	sudo /usr/lib/ckan/default/bin/paster --plugin=ckan datastore set-permissions postgres -c /usr/lib/ckan/default/src/ckan/test-core.ini
+echo 'Configure NFS mountpoint to access for development'
+echo '----------------------------------'
+sudo apt-get install --yes nfs-common
+sudo apt-get install --yes nfs-kernel-server
+sudo echo '/usr/lib/ckan *(rw,async,no_subtree_check,insecure,anonuid=0,anongid=0,all_squash)' | sudo tee -a /etc/exports
+sudo /etc/init.d/nfs-kernel-server restart
 
-	echo 'Configure NFS mountpoint to access for development'
-	echo '----------------------------------'
-	sudo apt-get install --yes nfs-common
-	sudo apt-get install --yes nfs-kernel-server
-	sudo echo '/usr/lib/ckan *(rw,async,no_subtree_check,insecure,anonuid=0,anongid=0,all_squash)' | sudo tee -a /etc/exports
-	sudo /etc/init.d/nfs-kernel-server restart
+echo 'Create CKAN test data'
+echo '----------------------------------'
+sudo /usr/lib/ckan/default/bin/paster --plugin=ckan create-test-data -c /etc/ckan/default/production.ini
 
-	echo 'Create CKAN test data'
-	echo '----------------------------------'
-	sudo /usr/lib/ckan/default/bin/paster --plugin=ckan create-test-data -c /etc/ckan/default/production.ini
-
-fi
