a9b73d6d9e8df087e8afe215d539f26a0d414845
Open-Steps.org <contact@open-steps.org>
2014-10-06 14:48:21 +0200

    wordpress.sh running. now provisioning scripts download odm-scripting release 0.3

--- deployment-scripts/vagrant/wordpress.sh
+++ deployment-scripts/vagrant/wordpress.sh
@@ -20,12 +20,16 @@ echo '----------------------------------'
 sudo apt-get install --yes git-core
 cd /vagrant/
 rm -rf odm-scripting
-git clone -b integrating-wp https://$github_user:$github_pass@github.com/OpenDevelopmentMekong/odm-scripting.git
+git clone -b odm-scripting-0.3 https://$github_user:$github_pass@github.com/OpenDevelopmentMekong/odm-scripting.git
+
+echo 'Install apache/nginx, if not yet installed'
+echo '----------------------------------'
+sudo apt-get install --yes apache2 libapache2-mod-wsgi nginx
 
 echo 'Install mysql server'
 echo '----------------------------------'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password odmadmin'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again odmadmin'
+echo 'mysql-server mysql-server/root_password password odmadmin' | sudo debconf-set-selections
+echo 'mysql-server mysql-server/root_password_again password odmadmin' | sudo debconf-set-selections
 sudo apt-get install --yes mysql-server
 
 echo 'Install PHP'
@@ -35,32 +39,34 @@ sudo apt-get install --yes php5 php5-mysql
 echo 'Install WP-CLI, http://wp-cli.org'
 echo '----------------------------------'
 mkdir /vagrant/wp-cli
-cd /vagrant/wp-cli
+cd /vagrant/wp-cli/
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
-wget https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
-echo "source /vagrant/wp-cli/wp-completion.bash" >> ~/.bash_profile
+sudo chown vagrant:vagrant /usr/local/bin/wp
+# wget https://github.com/wp-cli/wp-cli/raw/master/utils/wp-completion.bash
+# echo "source /vagrant/wp-cli/wp-completion.bash" >> /home/vagrant/.bash_profile
 
 echo 'Download WP version 4.0'
 echo '----------------------------------'
 sudo mkdir /var/www/wp
-sudo chown -R `whoami` /var/www/wp
-cd /var/www/wp
-wp core download --version=4.0
+cd /var/www/wp/
+wp core download --version=4.0 --allow-root
+sudo chown -R vagrant:vagrant /var/www/wp
 
 echo 'copy wp-config.php file from odm-scripting'
 echo '----------------------------------'
-sudo cp -fr /vagrant/odm-scripting/deployment-scripts/wp_deployment/wp-config.php /var/www/wp/wp-config.php
+cp -fr /vagrant/odm-scripting/deployment-scripts/wp_deployment/wp-config.php /var/www/wp/wp-config.php
+sudo chown -R vagrant:vagrant /var/www/wp/wp-config.php
 
-echo 'copy nginx/sites-available/wp from odm-scripting'
+echo 'copy apache2/sites-available/wp from odm-scripting'
 echo '----------------------------------'
 sudo cp -fr /vagrant/odm-scripting/deployment-scripts/wp_deployment/apache/wp /etc/apache2/sites-available/wp
 sudo ln -s /etc/apache2/sites-available/wp /etc/apache2/sites-enabled
 
-echo 'copy apache2/sites-available/wp from odm-scripting'
+echo 'copy nginx/sites-available/wp from odm-scripting'
 echo '----------------------------------'
-sudo cp -fr /vagrant/odm-scripting/deployment-scripts/wp_deployment/nginx/wp /etc/ngnix/sites-available/wp
+sudo cp -fr /vagrant/odm-scripting/deployment-scripts/wp_deployment/nginx/wp /etc/nginx/sites-available/wp
 sudo ln -s /etc/nginx/sites-available/wp /etc/nginx/sites-enabled
 
 echo 'Add virtual host on port 8081'
@@ -70,4 +76,11 @@ sudo echo "Listen 8081" >> /etc/apache2/ports.conf
 
 echo 'Create WP DB'
 echo '----------------------------------'
-wp db create
+cd /var/www/wp/
+wp db create --allow-root
+sudo chown -R vagrant:vagrant /var/www/wp
+
+echo 'Restart apache and nginx'
+echo '----------------------------------'
+sudo service apache2 reload
+# sudo service nginx reload
