75b477e7dcc5bb75347f8b5b771a3bec2109447a
Open-Steps.org <contact@open-steps.org>
2014-11-26 18:42:53 +0100

    Installing mishari's patched version of ansible Added privileged: false on Vagrantfile

--- deployment-scripts/vagrant/bootstrap.sh
+++ deployment-scripts/vagrant/bootstrap.sh
@@ -5,12 +5,19 @@
 # Make sure we're up to date
 apt-get update
 # Install Ansible
-apt-get install -y software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
-apt-get install -y ansible
+#apt-get install -y software-properties-common
+# apt-add-repository -y ppa:ansible/ansible
+# apt-get update
+# apt-get install -y ansible
+apt-get install -y git-core
+apt-get install -y python-setuptools
+git clone -b fix-synchronize-local https://github.com/mishari/ansible.git --recursive
+cd ./ansible
+source ./hacking/env-setup
+easy_install pip
+pip install paramiko PyYAML Jinja2 httplib2
 # Run playbook to provision up VM
 # This just installs git and checks out this repo
-sudo ansible-playbook /vagrant/ansible/bootstrap.yml -i /vagrant/ansible/stage --connection=local
+ansible-playbook /vagrant/ansible/bootstrap.yml -i /vagrant/ansible/stage --connection=local
 # Run Noop for a Proof of concept
 ansible-playbook /vagrant/ansible/site-vagrant.yml -i /vagrant/ansible/stage --connection=local
